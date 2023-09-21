fa221be5b1b5e3a99bb25ec8843026bb97aab3e0
gnuhub <gnuhub@gmail.com>
2023-09-21 22:39:32 +0000

    update template from 0002

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -74,10 +74,12 @@ if [ ! -z $${GITHUB_REPOSITORY} ];then
         echo $repo | tr -d '\r'
         export repo_name=$(echo $repo | tr -d '\r')
         echo $repo_name
-        export repo_name_length=$(expr length ${repo_name})
-        echo $repo_name_length
-        if [ $repo_name_length -gt 3 ];then
-            gh workflow run 1.ci.yml --repo $repo_name --ref $GITHUB_REF_NAME
+        if [ ! -z $repo_name ];then 
+            export repo_name_length=$(expr length ${repo_name})
+            echo $repo_name_length
+            if [ $repo_name_length -gt 3 ];then
+                gh workflow run 1.ci.yml --repo $repo_name --ref $GITHUB_REF_NAME
+            fi
         fi
     done < 8.workflows.to.run.repos.txt
 
