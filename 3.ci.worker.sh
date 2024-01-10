5ed0bfcf133d842be7854c3174e00f5c6316f28a
gnuhub <gnuhub@qq.com>
2024-01-10 12:14:44 +0000

    CI-BOT:2024.01.10-121444-main-985

--- 3.ci.worker.sh
+++ 3.ci.worker.sh
@@ -19,6 +19,27 @@ env
 echo "============================================================================"
 if [ ! -z "${GITHUB_REPOSITORY}" ];then
 
+sudo df -h
+sudo rm -rf /usr/local/lib/android # will release about 10 GB if you don't need Android
+sudo rm -rf /usr/share/dotnet # will release about 20GB if you don't need .NET
+
+# sudo docker image prune --all --force
+
+# https://github.com/jlumbroso/free-disk-space/blob/main/action.yml
+sudo rm -rf /opt/ghc
+
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk google-chrome-stable firefox powershell mono-devel libgl1-mesa-dri
+sudo apt-get autoremove -y
+sudo apt-get clean
+echo "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+sudo df -h
+
     # 0
     # sudo apt update -y
     # sudo apt upgrade -y
